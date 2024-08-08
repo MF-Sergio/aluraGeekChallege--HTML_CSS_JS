8f233dc42f348426caadbe25dc906a619fee7e66
Sérgio M. Filho <sergiohrznt@gmail.com>
2024-08-07 22:57:48 -0300

    :sparkles: função filtrar pesquisas na página principal

--- js/filtrar-pesquisa.js
+++ js/filtrar-pesquisa.js
@@ -1,10 +1,20 @@
 const inputPesquisa = document.querySelector("[data-pesquisa]");
+const items = document.querySelectorAll(".lista__item");
 
 const filtraPesquisa = () => {
-
-    
-
-
+    inputPesquisa.addEventListener('input', () => {
+        const valuePesquisa = inputPesquisa.value.toLowerCase();
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const itemtext = item.textContent || item.innerText;
+            if (itemtext.toLowerCase().includes(valuePesquisa)) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        }
+    })
 }
+
 
 export default filtraPesquisa;
